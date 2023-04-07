c9a45175b5152ba7104b2c93ff11041460018ae7
Rasulchek <rasulglash@gmail.com>
2023-04-07 17:48:06 +0300

    firs9

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
+from scipy import stats
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 680977959 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return x.mean()/24 # Ваш ответ
